2532b78120b99e9ab1a6975d2610441ab51eaee4
VinZVI <vinilzm@gmail.com>
2026-04-10 22:43:42 +0300

    Remove deprecated files: ETL workflow, database fix scripts, and test files

--- fix_dates.js
+++
@@ -1,5 +0,0 @@
-// Quick test to see how Date works with this
-const s = "Tue, 31 Mar 2026 00:05:00 GMT";
-console.log("Old way:", s.replace('T',' ').slice(0,19));
-const d = new Date(s);
-console.log("New way:", d.toISOString().replace('T', ' ').slice(0, 19));
